9e34460331775c0b09e3b8015ba5b91bfe8fddd9
alex960714 <alex_9607@mail.ru>
2017-12-09 01:58:26 +0300

    Added lost game handler

---
+++ Assets/Scripts/EOG.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EOG : MonoBehaviour {
+
+	public void NewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
